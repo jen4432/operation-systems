26c53dfb293e9d92872135a2feebfb72e51534dd
Zhenya <manixjen@gmail.com>
2022-12-30 18:34:29 +0300

    r/w_lock

--- Read-Write_lock/Read-Write lock/Source.cpp
+++ Read-Write_lock/Read-Write lock/Source.cpp
@@ -10,39 +10,45 @@ public:
     template<class Func>
     void read(Func func) {
         read_.lock();
-        blocked_readers_++;
-        mut_flag.lock();
-        flag = false;
-        mut_flag.unlock();
+        while (wraiter_waiting > 0 || writer_active) {
+            cv.wait();
+        }
+        readers_active++;
         read_.unlock();
         func();
         read_.lock();
-        blocked_readers_--;
-        read_.unlock();
-        if (blocked_readers_ == 0) {
+        readers_active--;
+        if (readers_active == 0) {
             cv.notify_one();
-            mut_flag.lock();
-            flag = true;
-            mut_flag.unlock();
         }
-        
+        read_.unlock();
     }
 
     template<class Func>
     void write(Func func) {
         unique_lock<mutex> lock(write_);
-        while (!flag) {
+        read_.lock();
+        wraiter_waiting--;
+        while (wraiter_waiting > 0 || writer_active == true) {
             cv.wait(lock);
         }
-        func();
+        wraiter_waiting--;
+        writer_active = true;
+        read_.unlock();
+        
+        read_.lock();
+        writer_active = false;
+        cv.notify_one();
+        read_.unlock();
     }
 
 private:
-    std::mutex mut_flag;
+    int readers_active = 0;
+    int wraiter_waiting = 0;
+    bool writer_active = false;
     std::mutex read_;
     std::mutex write_;
     int blocked_readers_ = 0;
-    bool flag = false;
     std::condition_variable cv;
 
 };
